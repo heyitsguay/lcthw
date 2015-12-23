39e14954edae3a074aab77ccf862578ccc2c3be3
Matt Guay <matt.d.guay@gmail.com>
2015-12-23 01:29:18 -0500

    Added examples 1, 3, 4, 6, 7.

---
+++ ex4/ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int age = 26;
+	int height;
+	
+	// Wrong on purpose.
+	printf("I am %d years old.\n");
+	printf("I am %d inches tall.\n", height);
+	
+	return 0;
+}
